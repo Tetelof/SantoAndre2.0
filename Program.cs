1cd751e1ccd0b44b1acc32d72634db97a48f9478
Tetelof <94086307+Tetelof@users.noreply.github.com>
2022-04-30 18:35:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SantoAndre
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(MDISigleton.GetMDIParent());
+        }
+    }
+}
